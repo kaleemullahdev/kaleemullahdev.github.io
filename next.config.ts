f753ca143ea2c8849dd1e21bbf622c4815dec8e3
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2025-06-24 23:21:42 +0500

    Master: Adds path fix

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,14 @@ import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
 const nextConfig = (phase: string) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER
   const nextConfig = {
-    output: 'export',
-    assetPrefix: isDev ? undefined : '/kaleem-ullah',
+    // output: 'export',
+    assetPrefix: isDev ? undefined : '',
     images: {
       unoptimized: isDev ? true : false,
       loader: isDev ? 'default' : 'custom',
       loaderFile: isDev ? undefined : './src/utils/imageLoader.ts',
     },
-    basePath: isDev ? '' : '/kaleem-ullah',
+    basePath: isDev ? '' : '',
   }
 
   return nextConfig
